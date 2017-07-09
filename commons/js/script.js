a4b55c501b3c643a1d530449426556e2fdfb1591
Roman Vasilev <zvezdarusy@gmail.com>
2017-07-09 19:30:50 +0600

    created about, counts and services blocks for desktop and mobile devices

--- commons/js/script.js
+++ commons/js/script.js
@@ -24,3 +24,31 @@ var main = function() {
     });
 };
 $(document).ready(main);
+
+/* Count-block */
+$(document).ready(function(){
+    var show = true;
+    var countbox = "#counts";
+    $(window).on("scroll load resize", function(){
+
+        if(!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
+
+        var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+
+        var w_height = $(window).height();        // Высота окна браузера
+        var d_height = $(document).height();      // Высота всего документа
+
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+
+        if(w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+            $(".spincrement").spincrement({
+                thousandSeparator: "",
+                duration: 4000
+            });
+
+            show = false;
+        }
+    });
+});
+/* Count-block */
